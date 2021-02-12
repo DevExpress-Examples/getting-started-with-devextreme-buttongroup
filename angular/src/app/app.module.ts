3a63393af299be3fa40667e1076f16ef44f787dc
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-12 19:06:30 +0300

    fix imports

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
-import { DxButtonGroupModule } from 'devextreme-angular';
+import { DxButtonGroupModule } from 'devextreme-angular/ui/button-group';
 
 @NgModule({
     declarations: [
